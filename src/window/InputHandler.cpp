2d0a6a661d5101bcb75ca10a1f35eb63c746975d
Tobias G <31646748+Daikalos@users.noreply.github.com>
2022-08-05 20:28:31 +0200

    small fix

--- src/window/InputHandler.cpp
+++ src/window/InputHandler.cpp
@@ -3,20 +3,13 @@
 using namespace fge;
 
 InputHandler::InputHandler()
+	: _scroll_delta(0.0f)
 {
 	memset(_current_button_state, false, sizeof(_current_button_state));
 	memset(_previous_button_state, false, sizeof(_previous_button_state));
 
 	memset(_current_key_state, false, sizeof(_current_key_state));
 	memset(_previous_key_state, false, sizeof(_previous_key_state));
-
-	for (unsigned short i = 0; i < sf::Mouse::ButtonCount; ++i)
-		_button_bindings[i] = static_cast<sf::Mouse::Button>(i);
-
-	for (unsigned short i = 0; i < sf::Keyboard::KeyCount; ++i)
-		_key_bindings[i] = static_cast<sf::Keyboard::Key>(i);
-
-	_scroll_delta = 0.0f;
 }
 
 InputHandler::~InputHandler()
@@ -29,13 +22,13 @@ bool InputHandler::update()
 	for (unsigned short i = 0; i < sf::Mouse::ButtonCount; ++i)
 	{
 		_previous_button_state[i] = _current_button_state[i];
-		_current_button_state[i] = sf::Mouse::isButtonPressed(_button_bindings[i]);
+		_current_button_state[i] = sf::Mouse::isButtonPressed(static_cast<sf::Mouse::Button>(i));
 	}
 
 	for (unsigned short i = 0; i < sf::Keyboard::KeyCount; ++i)
 	{
 		_previous_key_state[i] = _current_key_state[i];
-		_current_key_state[i] = sf::Keyboard::isKeyPressed(_key_bindings[i]);
+		_current_key_state[i] = sf::Keyboard::isKeyPressed(static_cast<sf::Keyboard::Key>(i));
 	}
 
 	_scroll_delta = 0.0f;
--- src/window/InputHandler.h
+++ src/window/InputHandler.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <unordered_map>
 
 namespace fge
 {
@@ -17,20 +18,27 @@ namespace fge
 		bool update();
 
 	public:
-		inline bool get_left_pressed() const { return _current_button_state[sf::Mouse::Button::Left] && !_previous_button_state[sf::Mouse::Button::Left]; }
-		inline bool get_right_pressed() const { return _current_button_state[sf::Mouse::Button::Right] && !_previous_button_state[sf::Mouse::Button::Right]; }
-		inline bool get_middle_pressed() const { return _current_button_state[sf::Mouse::Button::Middle] && !_previous_button_state[sf::Mouse::Button::Middle]; }
+		inline bool get_button_pressed(sf::Mouse::Button button)
+		{
+			if (button < 0 || button >= sf::Mouse::ButtonCount)
+				return false;
 
-		inline bool get_left_released() const { return !_current_button_state[sf::Mouse::Button::Left] && _previous_button_state[sf::Mouse::Button::Left]; }
-		inline bool get_right_released() const { return !_current_button_state[sf::Mouse::Button::Right] && _previous_button_state[sf::Mouse::Button::Right]; }
-		inline bool get_middle_released() const { return !_current_button_state[sf::Mouse::Button::Middle] && _previous_button_state[sf::Mouse::Button::Middle]; }
+			return _current_button_state[button] && !_previous_button_state[button];
+		}
+		inline bool get_button_released(sf::Mouse::Button button)
+		{
+			if (button < 0 || button >= sf::Mouse::ButtonCount)
+				return false;
 
-		inline bool get_left_held() const { return _current_button_state[sf::Mouse::Button::Left] && _previous_button_state[sf::Mouse::Button::Left]; }
-		inline bool get_right_held() const { return _current_button_state[sf::Mouse::Button::Right] && _previous_button_state[sf::Mouse::Button::Right]; }
-		inline bool get_middle_held() const { return _current_button_state[sf::Mouse::Button::Middle] && _previous_button_state[sf::Mouse::Button::Middle]; }
+			return !_current_button_state[sf::Mouse::Button::Left] && _previous_button_state[sf::Mouse::Button::Left];
+		}
+		inline bool get_button_held(sf::Mouse::Button button)
+		{
+			if (button < 0 || button >= sf::Mouse::ButtonCount)
+				return false;
 
-		inline bool get_scroll_up() const { return _scroll_delta > 0; }
-		inline bool get_scroll_down() const { return _scroll_delta < 0; }
+			return _current_button_state[sf::Mouse::Button::Left] && _previous_button_state[sf::Mouse::Button::Left];
+		}
 
 		inline bool get_key_pressed(sf::Keyboard::Key key) const
 		{
@@ -39,7 +47,6 @@ namespace fge
 
 			return (_current_key_state[key] && !_previous_key_state[key]);
 		}
-
 		inline bool get_key_released(sf::Keyboard::Key key) const
 		{
 			if (key < 0 || key >= sf::Keyboard::Key::KeyCount)
@@ -47,7 +54,6 @@ namespace fge
 
 			return (!_current_key_state[key] && _previous_key_state[key]);
 		}
-
 		inline bool get_key_held(sf::Keyboard::Key key) const
 		{
 			if (key < 0 || key >= sf::Keyboard::Key::KeyCount)
@@ -56,9 +62,30 @@ namespace fge
 			return _current_key_state[key] && _previous_key_state[key];
 		}
 
+		inline bool get_key_pressed(std::string name)	  { return _key_bindings.contains(name) && get_key_pressed(_key_bindings[name]); }
+		inline bool get_button_pressed(std::string name)  { return _button_bindings.contains(name) && get_button_pressed(_button_bindings[name]); }
+
+		inline bool get_key_released(std::string name)	  { return _key_bindings.contains(name) && get_key_released(_key_bindings[name]); }
+		inline bool get_button_released(std::string name) { return _button_bindings.contains(name) && get_button_released(_button_bindings[name]); }
+
+		inline bool get_key_held(std::string name)		  { return _key_bindings.contains(name) && get_key_held(_key_bindings[name]); }
+		inline bool get_button_held(std::string name)	  { return _button_bindings.contains(name) && get_button_held(_button_bindings[name]); }
+
+		inline bool get_scroll_up() const	{ return _scroll_delta > 0; }
+		inline bool get_scroll_down() const { return _scroll_delta < 0; }
+
 		void set_scroll_delta(float scroll_delta)
 		{
 			_scroll_delta = scroll_delta;
+		}
+
+		void set_key_binding(std::string name, sf::Keyboard::Key key)
+		{
+			_key_bindings[name] = key;
+		}
+		void set_button_binding(std::string name, sf::Mouse::Button button)
+		{
+			_button_bindings[name] = button;
 		}
 
 	private:
@@ -68,10 +95,10 @@ namespace fge
 		bool _current_key_state[sf::Keyboard::KeyCount];
 		bool _previous_key_state[sf::Keyboard::KeyCount];
 
-		sf::Mouse::Button _button_bindings[sf::Mouse::ButtonCount];
-		sf::Keyboard::Key _key_bindings[sf::Keyboard::KeyCount];
-
 		float _scroll_delta;
+
+		std::unordered_map<std::string, sf::Keyboard::Key> _key_bindings;
+		std::unordered_map<std::string, sf::Mouse::Button> _button_bindings;
 	};
 }
 
